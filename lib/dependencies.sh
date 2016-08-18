70b6d7db8ff8a9dd32f234194d5dc79abb3be115
Daniel Greenberg <daniel@hirefrederick.com>
2016-08-18 14:05:54 -0700

    echo npm config list

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -20,6 +20,7 @@ install_node_modules() {
     fi
     echo "daniel rox"
     cat $build_dir/.npmrc
+    npm config list
     npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
   else
     echo "Skipping (no package.json)"
